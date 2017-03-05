3a51076680d9c32b62b0b957fa622f8bb1e00d6f
Sergey <grishy.ru@yandex.ru>
2017-03-05 19:14:05 +0300

    optimize 03

--- 03/main.js
+++ 03/main.js
@@ -1,8 +1,9 @@
 'use strict'
-const CIRCLE = Math.PI * 2;
-const degreesToRadians = (degrees) => degrees * Math.PI / 180;
+var CIRCLE = Math.PI * 2;
+var degreesToRadians = (degrees) => degrees * Math.PI / 180;
+var fastCeil = x => x == (x=x|0) ? x : x+1;
 
-const level = [
+var level = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -35,7 +36,7 @@ const level = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ];
 
-const WORLD = {
+var WORLD = {
     sizeBlock: 24,
     playerRadius: 6
 }
@@ -59,7 +60,7 @@ class Control {
     }
 
     onKey(val, e) {
-        let state = this.codes[e.keyCode];
+        var state = this.codes[e.keyCode];
         if (typeof state === 'undefined') return;
         this.status[state] = val;
         e.preventDefault && e.preventDefault();
@@ -77,12 +78,12 @@ class Player {
     }
 
     walk(distance) {
-        let dx = Math.cos(this.direction) * distance;
-        let dy = Math.sin(this.direction) * distance;
+        var dx = Math.cos(this.direction) * distance;
+        var dy = Math.sin(this.direction) * distance;
 
-        let radiusPlaterInMap = WORLD.playerRadius / WORLD.sizeBlock;
-        let collisionX = dx > 0 ? radiusPlaterInMap : -radiusPlaterInMap;
-        let collisionY = dy > 0 ? radiusPlaterInMap : -radiusPlaterInMap;
+        var radiusPlaterInMap = WORLD.playerRadius / WORLD.sizeBlock;
+        var collisionX = dx > 0 ? radiusPlaterInMap : -radiusPlaterInMap;
+        var collisionY = dy > 0 ? radiusPlaterInMap : -radiusPlaterInMap;
 
         if (map.collision(this.x + collisionX, this.y) <= 0) this.x += dx;
         if (map.collision(this.x, this.y + collisionY) <= 0) this.y += dy;
@@ -107,48 +108,48 @@ class Map {
     }
 
     castRay(angle, range) {
-        const sin = Math.sin(angle);
-        const cos = Math.cos(angle);
-        const tan = sin / cos;
-        const cot = cos / sin;
-        const cosNegative = cos < 0;
-        const sinNegative = sin < 0;
+        var sin = Math.sin(angle);
+        var cos = Math.cos(angle);
+        var tan = sin / cos;
+        var cot = cos / sin;
+        var cosNegative = cos < 0;
+        var sinNegative = sin < 0;
 
-        let currentX = player.x;
-        let currentY = player.y;
-        let hit = [];
+        var currentX = player.x;
+        var currentY = player.y;
+        var hit = [];
 
-        let distance = 0;
+        var distance = 0;
 
         while (distance < range) {
             // Collision with the nearest axis X
-            let dxx = cosNegative ?
-                Math.ceil(currentX - 1) - currentX :
-                Math.floor(currentX + 1) - currentX;
-            let dxy = dxx * tan;
-            let lengthX2 = dxx * dxx + dxy * dxy;
+            var dxx = cosNegative ?
+                fastCeil(currentX - 1) - currentX :
+                ((currentX + 1) << 0) - currentX; // Math.floor
+            var dxy = dxx * tan;
+            var lengthX2 = dxx * dxx + dxy * dxy;
             // Collision with the nearest axis Y
-            let dyx = sinNegative ?
-                Math.ceil(currentY - 1) - currentY :
-                Math.floor(currentY + 1) - currentY;
-            let dyy = dyx * cot;
-            let lengthY2 = dyx * dyx + dyy * dyy;
+            var dyx = sinNegative ?
+                fastCeil(currentY - 1) - currentY :
+                ((currentY + 1) << 0) - currentY; // Math.floor
+            var dyy = dyx * cot;
+            var lengthY2 = dyx * dyx + dyy * dyy;
 
-            let collision = 0;
+            var collision = 0;
 
             if (lengthX2 < lengthY2) {
-                currentX += dxx;
-                currentY += dxy;
-                distance += Math.sqrt(lengthX2);
+                currentX = currentX + dxx;
+                currentY = currentY + dxy;
+                distance = distance + Math.sqrt(lengthX2);
 
-                let shift = cosNegative ? 1 : 0;
+                var shift = cosNegative ? 1 : 0;
                 collision = this.collision(currentX - shift, currentY);
             } else {
-                currentX += dyy;
-                currentY += dyx;
-                distance += Math.sqrt(lengthY2);
+                currentX = currentX + dyy;
+                currentY = currentY + dyx;
+                distance = distance + Math.sqrt(lengthY2);
 
-                let shift = sinNegative ? 1 : 0;
+                var shift = sinNegative ? 1 : 0;
                 collision = this.collision(currentX, currentY - shift);
             }
 
@@ -162,8 +163,8 @@ class Map {
     }
 
     collision(x, y) {
-        x = Math.floor(x);
-        y = Math.floor(y);
+        x = x << 0; // Math.floor
+        y = y << 0; // Math.floor
         if (x < 0 || x > this.width - 1 || y < 0 || y > this.height - 1) return -1;
         return level[x][y];
     }
@@ -174,7 +175,7 @@ class Camera {
         this.width = map.width * WORLD.sizeBlock;
         this.height = map.height * WORLD.sizeBlock;
         this.focalLength = 0.8;
-        this.resolution = 200;
+        this.resolution = 250;
         this.rayLength = 40;
     }
 
@@ -188,10 +189,10 @@ class Camera {
     }
 
     renderRays() {
-        for (let column = 0; column < this.resolution; column++) {
+        for (var column = 0; column < this.resolution; column++) {
             // -0.5 < x < 0.5
-            let x = column / this.resolution - 0.5;
-            let angle = player.direction + Math.atan2(x, this.focalLength);
+            var x = column / this.resolution - 0.5;
+            var angle = player.direction + Math.atan2(x, this.focalLength);
 
             var ray = map.castRay(angle, this.rayLength);
             this.drawRay(
@@ -205,23 +206,23 @@ class Camera {
     }
 
     renderPlayer() {
-        let x = player.x * WORLD.sizeBlock;
-        let y = player.y * WORLD.sizeBlock;
+        var x = player.x * WORLD.sizeBlock;
+        var y = player.y * WORLD.sizeBlock;
 
         ctx.fillStyle = "#F4511E";
         ctx.beginPath();
         ctx.arc(x, y, WORLD.playerRadius, 0, CIRCLE, false);
         ctx.fill();
 
-        let lineEndX = x + Math.cos(player.direction) * 40;
-        let lineEndY = y + Math.sin(player.direction) * 40;
+        var lineEndX = x + Math.cos(player.direction) * 40;
+        var lineEndY = y + Math.sin(player.direction) * 40;
         this.drawRay(x, y, lineEndX, lineEndY, '#E64A19')
     }
 
     renderMap() {
-        for (let y = 0; y < map.width; y++) {
-            for (let x = 0; x < map.height; x++) {
-                let wall = level[x][y];
+        for (var y = 0; y < map.width; y++) {
+            for (var x = 0; x < map.height; x++) {
+                var wall = level[x][y];
 
                 ctx.fillStyle = "#BDBDBD";
                 if (wall > 0) {
@@ -256,24 +257,24 @@ class GameLoop {
         requestAnimationFrame(this.frame);
     }
     frame(time) {
-        let seconds = (time - this.lastTime) / 1000;
+        var seconds = (time - this.lastTime) / 1000;
         this.lastTime = time;
         this.callback(seconds);
         requestAnimationFrame(this.frame);
     };
 }
 
-let canvas = document.getElementById("display");
-let ctx = canvas.getContext("2d");
-let map = new Map();
-let player = new Player(15, 10, degreesToRadians(90));
-let сamera = new Camera();
-let controls = new Control();
+var canvas = document.getElementById("display");
+var ctx = canvas.getContext("2d");
+var map = new Map();
+var player = new Player(15, 10, degreesToRadians(90));
+var сamera = new Camera();
+var controls = new Control();
 
 canvas.width = сamera.width;
 canvas.height = сamera.height;
 
-let loop = new GameLoop();
+var loop = new GameLoop();
 
 window.addEventListener("load", loop.start(function(seconds) {
     player.update(seconds);
